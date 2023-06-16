37f70c9d1fb1c02782dc8e5b79bfa4d291a218f6
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-17 00:14:30 +0530

    Added login router

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../model/user.model');
 const router = express.Router();
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 
 router.route('/register').post(async (req, res)=>{
@@ -20,5 +21,25 @@ router.route('/register').post(async (req, res)=>{
         res.status(500).json({message: "Error creating the new user."});
     }
 });
+
+router.route('/login').post(async (req, res)=>{
+    try{
+        const user = await User.findOne({number: req.body.number});
+        !user && res.status(401).json({message: "Invalid mobile number"});
+
+        const decodedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET_KEY).toString(CryptoJS.enc.Utf8);
+        decodedPassword!==req.body.password && res.status(401).json({message: "Password incorrect!"});
+
+        const {password, ...rest} = user._doc;
+        const accessToken = jwt.sign({username: user.username}, process.env.ACCESS_TOKEN);
+
+        //res.json(user);
+        //To hide the encrypted password from being displayed on console.
+        res.json({...rest, accessToken});
+    }
+    catch(err){
+        console.log(err);
+    }
+})
 
 module.exports = router;
