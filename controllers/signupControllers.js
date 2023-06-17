510e6e5b52e9a243b65e0acf7d72bc4873d458a6
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-17 14:24:00 +0530

    Created Signup Controller.

---
+++ controllers/signupControllers.js
@@ -0,0 +1,21 @@
+const CryptoJS = require('crypto-js');
+const User = require('../model/user.model');
+
+const signupHandler = (async (req, res)=>{
+    try{
+        const userObject = {
+            username: req.body.username,
+            email: req.body.email,
+            number: req.body.number,
+            password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET_KEY).toString()  
+        };
+        const newUser = new User(userObject);
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }
+    catch(err){
+        res.status(500).json({message: "Error creating the new user."});
+    }
+})
+
+module.exports = signupHandler;
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -4,23 +4,10 @@ const router = express.Router();
 const CryptoJS = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+const signupHandler = require('../controllers/signupControllers');
 
-router.route('/register').post(async (req, res)=>{
-    try{
-        const userObject = {
-            username: req.body.username,
-            email: req.body.email,
-            number: req.body.number,
-            password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET_KEY).toString()  
-        };
-        const newUser = new User(userObject);
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    }
-    catch(err){
-        res.status(500).json({message: "Error creating the new user."});
-    }
-});
+
+router.route('/register').post(signupHandler);
 
 router.route('/login').post(async (req, res)=>{
     try{
