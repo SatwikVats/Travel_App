c97ed0fbe7d2ef24d8f4dada62d8e31061df7441
SatwikVats <f20190194@pilani.bits-pilani.ac.in>
2023-06-18 16:27:18 +0530

    User signup timestamp.

--- model/user.model.js
+++ model/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//Schema of user
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true},
     email: {type: String, required: true, unique: true},
